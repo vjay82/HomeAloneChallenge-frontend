b81f706324a6900e5abc24b6957d5cf848049e5e
Malte <harryhaller001@gmail.com>
2020-03-21 18:07:07 +0100

    add classes folder

---
+++ src/app/classes/challenge.ts
@@ -0,0 +1,11 @@
+
+export class Challenge {
+    id: String;
+    title: String;
+    desciripion: String;
+    category: number;
+    duration: number;
+    revardPoints: number;
+    imageURL: String;
+    additionalLink: String;
+}
