e6bc64148d17a42bd07026203e8f429932271343
Volker Gronau <Volker.Gronau@dimensiondata.com>
2020-03-21 23:28:40 +0100

    fixed location error

--- src/app/select-challenge-page/select-challenge-page.component.ts
+++ src/app/select-challenge-page/select-challenge-page.component.ts
@@ -1,15 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Location } from "@angular/common";
 
 @Component({
-  selector: 'app-select-challenge-page',
-  templateUrl: './select-challenge-page.component.html',
-  styleUrls: ['./select-challenge-page.component.scss']
+  selector: "app-select-challenge-page",
+  templateUrl: "./select-challenge-page.component.html",
+  styleUrls: ["./select-challenge-page.component.scss"]
 })
 export class SelectChallengePageComponent implements OnInit {
+  constructor(public location: Location) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
