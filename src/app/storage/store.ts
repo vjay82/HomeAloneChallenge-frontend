a656593758f1f779a690e2d03a97e8f791fd6bab
Malte <harryhaller001@gmail.com>
2020-03-21 14:48:39 +0100

    add persistent storage class

---
+++ src/app/storage/store.ts
@@ -0,0 +1,35 @@
+import { PersistenceService, StorageType } from 'angular-persistence';
+import { Challenge } from '../api-service/challenge';
+
+export class Store {
+    private userId: String;
+    private activeChallenge: Challenge;
+
+    constructor(private persistenceService: PersistenceService) {
+        this.userId = this.persistenceService.get('USER_ID', StorageType.IMMUTABLE_MEMORY);
+        this.activeChallenge = this.persistenceService.get('CHALLENGE', StorageType.MEMORY);
+    }
+
+    setChallenge(challenge: Challenge) {
+        if (!this.persistenceService.set('CHALLENGE', {type: StorageType.MEMORY})) {
+            console.log("Cant set active challenge!");
+
+        }
+    }
+
+    getActiveChallenge() {
+        return this.activeChallenge;
+    }
+
+    getUserId() {
+        return this.userId;
+    }
+
+    setUserId(userId:String) {
+        if (!this.persistenceService.set('USER_ID', userId, {type: StorageType.IMMUTABLE_MEMORY})) {
+            // TODO: error handling!
+
+            console.log("Cant set local storage");
+        }        
+    }
+}
