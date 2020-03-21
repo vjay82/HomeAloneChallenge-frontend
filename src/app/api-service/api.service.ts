b75888e90fbbca3bd37a10cb72d25265225c49d3
Malte <harryhaller001@gmail.com>
2020-03-21 13:39:11 +0100

    Add api service (work in progress)

--- src/app/api-service/api.service.ts
+++ src/app/api-service/api.service.ts
@@ -1,11 +1,27 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Challenge } from './challenge';
+import { Tipp } from './tipp';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor() { }
+  apiURL: string = "http://localhost/api"
 
-  getDailyChallenge() {}
+  constructor(private httpClient: HttpClient) { 
+
+  }
+
+  getRandomChallenge() {
+    return this.httpClient.get<Challenge>(`${this.apiURL}/random/challenge`);
+  }
+
+  getRandomTip() {
+    return this.httpClient.get<Tipp>(`${this.apiURL}/random/tipp`);
+  }
+
+
 }
