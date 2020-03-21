2ba891393674afdedc1102180a08dbc9dcd8c333
Volker Gronau <Volker.Gronau@dimensiondata.com>
2020-03-21 20:55:17 +0100

    me playing around

--- src/app/api-service/api.service.ts
+++ src/app/api-service/api.service.ts
@@ -1,43 +1,41 @@
-import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
 
-import { Challenge } from '../classes/challenge';
-import { Tipp } from '../classes/tipp';
-import { Store } from '../storage/store';
-import { UserData } from '../classes/user-data';
-import { error } from 'protractor';
-import { Observable } from 'rxjs';
-import { resolve } from 'dns';
-import { rejects } from 'assert';
-
+import { Challenge } from "../classes/challenge";
+import { Tipp } from "../classes/tipp";
+import { Store } from "../storage/store";
+import { UserData } from "../classes/user-data";
+import { error } from "protractor";
+import { Observable } from "rxjs";
+import { resolve } from "dns";
+import { rejects } from "assert";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class ApiService {
+  apiURL: string = "/assets/demo-backend/api/v1";
 
-  apiURL: string = "https://home-alone-challenge.herokuapp.com/api/v1"
-
-  constructor(private httpClient: HttpClient, private storage: Store) { 
-
-  }
+  constructor(private httpClient: HttpClient, private storage: Store) {}
 
   checkUserId(): Promise<UserData> {
     return new Promise((resolve, reject) => {
-      console.log(this.storage.getUserId())
+      console.log(this.storage.getUserId());
       if (this.storage.getUserId() == undefined) {
         // Do API request toget UID
-        this.createUniqueId().subscribe((data: UserData) => {
-          this.storage.setUserId(data);
-          resolve(data)
-        }, (error) => {
-          reject("Canot access userId! " + error.message);
-        });
+        this.createUniqueId().subscribe(
+          (data: UserData) => {
+            this.storage.setUserId(data);
+            resolve(data);
+          },
+          error => {
+            reject("Canot access userId! " + error.message);
+          }
+        );
       } else {
-        resolve(this.storage.getUserId())
+        resolve(this.storage.getUserId());
       }
-      
-    })  
+    });
   }
 
   getDailyTip() {
@@ -48,25 +46,33 @@ export class ApiService {
     return this.httpClient.post<UserData>(`${this.apiURL}/users`, null);
   }
 
-  fetchAllChallenges() {    
-    return this.httpClient.get<Challenge[]>(`${this.apiURL}/users/${this.storage.getUserId()}/challenges`);
+  fetchAllChallenges() {
+    return this.httpClient.get<Challenge[]>(
+      `${this.apiURL}/users/${this.storage.getUserId()}/challenges`
+    );
   }
 
   createNewChallenge(challenge: Challenge) {
-    return this.httpClient.post(`${this.apiURL}/users/${this.storage.getUserId()}/challenges`, challenge);
+    return this.httpClient.post(
+      `${this.apiURL}/users/${this.storage.getUserId()}/challenges`,
+      challenge
+    );
   }
 
   getChallenge(challengeId: String) {
-    return this.httpClient.get<Challenge>(`${this.apiURL}/users/${this.storage.getUserId()}/challenges/${challengeId}`)
+    return this.httpClient.get<Challenge>(
+      `${
+        this.apiURL
+      }/users/${this.storage.getUserId()}/challenges/${challengeId}`
+    );
   }
 
   getRandomChallenge() {
-    return this.httpClient.get<Challenge>(`${this.apiURL}/users/${this.storage.getUserId()}/challenges/`)
-  } 
-
-
+    return this.httpClient.get<Challenge>(
+      `${this.apiURL}/users/${this.storage.getUserId()}/challenges/`
+    );
+  }
 }
-
 
 /*
 this.api.checkUserId().then(data => {
--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { ApiService } from '../api-service/api.service';
+import { ApiService } from "../api-service/api.service";
 
 @Component({
   selector: "app-main-page",
@@ -10,11 +10,8 @@ export class MainPageComponent implements OnInit {
   constructor(private api: ApiService) {}
 
   ngOnInit(): void {
-    this.api.checkUserId().then(data => {
-      // Do futher requests
-    }).catch(err => {
-      console.error(err)
-    })
-
+    this.api.getDailyTip().subscribe(data => {
+      console.log("tip:" + data);
+    });
   }
 }
