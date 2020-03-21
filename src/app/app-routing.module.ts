7ebc2a3b8b927c02ad85ff320b36ffc285a2ca02
Volker Gronau <Volker.Gronau@dimensiondata.com>
2020-03-21 19:38:48 +0100

    added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { GreetingPageComponent } from "./greeting-page/greeting-page.component";
 import { MainPageComponent } from "./main-page/main-page.component";
+import { FeedbackPageComponent } from "./feedback-page/feedback-page.component";
 import { ChallengePageComponent } from "./challenge-page/challenge-page.component";
-import { ProposalPageComponent } from "./proposal-page/proposal-page.component";
-import {ChallengeCompletedPageComponent} from "./challenge-completed-page/challenge-completed-page.component";
+import { HelpPageComponent } from "./help-page/help-page.component";
+import { CreateChallengePageComponent } from "./create-challenge-page/create-challenge-page.component";
+import { ChallengeCompletedPageComponent } from "./challenge-completed-page/challenge-completed-page.component";
+import { ChallengeCanceledPageComponent } from "./challenge-canceled-page/challenge-canceled-page.component";
 
 const routes: Routes = [
   { path: "", component: GreetingPageComponent },
   { path: "main", component: MainPageComponent },
   { path: "challenge", component: ChallengePageComponent },
-  { path: 'challenge/completed', component: ChallengeCompletedPageComponent },
-  { path: "proposal", component: ProposalPageComponent }
+  { path: "challenge/completed", component: ChallengeCompletedPageComponent },
+  { path: "challenge/canceled", component: ChallengeCanceledPageComponent },
+  { path: "challenge/create", component: CreateChallengePageComponent },
+  { path: "feedback", component: FeedbackPageComponent },
+  { path: "help", component: HelpPageComponent }
 ];
 
 @NgModule({
