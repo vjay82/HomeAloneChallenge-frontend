8932fab560f8b7f964be716aded8bd151c7132f9
Volker Gronau <Volker.Gronau@dimensiondata.com>
2020-03-21 19:10:33 +0100

    fixed build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,14 @@
-import { Component } from '@angular/core';
-import {ApiService} from "./api-service/api.service";
-import { Challenge } from './classes/challenge';
+import { Component } from "@angular/core";
+
+import { Challenge } from "./classes/challenge";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"]
 })
 export class AppComponent {
-  title = 'HomeAloneChallengeFrontend';
+  title = "HomeAloneChallengeFrontend";
 
-  constructor(
-    private apiService: ApiService,
-  ) {
-    this.apiService.getRandomChallenge("test").subscribe((data: Challenge) => console.log(data));
-  }
-
-
+  constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,11 @@ import { AppComponent } from "./app.component";
 import { MainPageComponent } from "./main-page/main-page.component";
 import { ChallengePageComponent } from "./challenge-page/challenge-page.component";
 import { ProposalPageComponent } from "./proposal-page/proposal-page.component";
-import { ApiService } from "./api-service/api.service";
-import { HttpClientModule } from '@angular/common/http';
-import { GreetingPageComponent } from './greeting-page/greeting-page.component';
-import { PersistenceModule } from 'angular-persistence';
-import { ChallengeCompletedPageComponent } from './challenge-completed-page/challenge-completed-page.component';
 
+import { HttpClientModule } from "@angular/common/http";
+import { GreetingPageComponent } from "./greeting-page/greeting-page.component";
+import { PersistenceModule } from "angular-persistence";
+import { ChallengeCompletedPageComponent } from "./challenge-completed-page/challenge-completed-page.component";
 
 @NgModule({
   declarations: [
@@ -21,16 +20,16 @@ import { ChallengeCompletedPageComponent } from './challenge-completed-page/chal
     ChallengePageComponent,
     ProposalPageComponent,
     GreetingPageComponent,
-    ChallengeCompletedPageComponent,
+    ChallengeCompletedPageComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     NgbModule,
     HttpClientModule,
-    PersistenceModule,
+    PersistenceModule
   ],
-  providers: [ApiService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
