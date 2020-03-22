c0af1dc49960e6d05d589914d73bf8a70776c35b
Malte <harryhaller001@gmail.com>
2020-03-22 14:50:07 +0100

    edit trees

--- src/app/revard-tree-page/revard-tree-page.component.ts
+++ src/app/revard-tree-page/revard-tree-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-revard-tree-page',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RevardTreePageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public location: Location) { }
 
   ngOnInit(): void {
   }
