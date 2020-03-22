433c749fee748f87b28b3aa7fc6d4cdb13abdfca
Volker Gronau <Volker.Gronau@dimensiondata.com>
2020-03-22 09:58:06 +0100

    set challenge and navigate to it

--- src/app/select-challenge-page/select-challenge-page.component.ts
+++ src/app/select-challenge-page/select-challenge-page.component.ts
@@ -3,6 +3,8 @@ import { Location } from "@angular/common";
 import { ApiService } from "../api-service/api.service";
 import { Challenge } from "../classes/challenge";
 import { Category } from "../classes/category";
+import { Store } from "../storage/store";
+import { RouterModule, Router } from "@angular/router";
 
 @Component({
   selector: "app-select-challenge-page",
@@ -13,9 +15,21 @@ export class SelectChallengePageComponent implements OnInit {
   public dailyChallenge$: Promise<Challenge>;
   public categories: string[];
 
-  constructor(private api: ApiService, public location: Location) {
+  constructor(
+    private router: Router,
+    private api: ApiService,
+    public location: Location,
+    private store: Store
+  ) {
     this.dailyChallenge$ = this.api.getDailyChallenge();
     this.categories = Object.keys(Category);
+  }
+
+  public selectCategory(category: string) {
+    this.api.getRandomChallenge(category).then(challenge => {
+      this.store.setChallenge(challenge);
+      this.router.navigate([`/challenge`]);
+    });
   }
 
   ngOnInit(): void {}
