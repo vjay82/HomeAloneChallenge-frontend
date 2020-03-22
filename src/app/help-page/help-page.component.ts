4b09296deb233c59cfd356c649e62f8edf25fd8f
Malte <harryhaller001@gmail.com>
2020-03-22 04:07:44 +0100

    add help page

--- src/app/help-page/help-page.component.ts
+++ src/app/help-page/help-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from "@angular/common";
 
 @Component({
   selector: 'app-help-page',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HelpPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(public location: Location) { }
 
   ngOnInit(): void {
   }
